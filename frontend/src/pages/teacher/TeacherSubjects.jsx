00a6a191f8faa15efe92302679f2aa995190359f
RaiyanChandle <rrchandle@gmail.com>
2025-09-15 19:06:56 +0530

    added teacher pages and routes

---
+++ frontend/src/pages/teacher/TeacherSubjects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const TeacherSubjects = () => (
+  <div className="flex flex-1 items-center justify-center text-2xl text-violet-400 font-bold h-full">
+    Teacher Subjects Page
+  </div>
+);
+export default TeacherSubjects;
